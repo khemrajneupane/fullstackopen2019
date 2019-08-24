3c739bb8f30da25d341409a01f743773821c3acc
NeupaneKhemRaj <shekharneupane@gmail.com>
2019-08-24 16:43:28 +0300

    upto Ex-4.21, final of part4

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -35,7 +35,7 @@ blogsRouter.post('/', async (req, res, next) => {
 
     //console.log(decodedToken);
     if (!tokenExtractor.tokenExtractor(req) || !decodedToken.id) {
-      res.status(400).send({ error: 'bad username or password' })
+      res.status(400).send({ error: 'Incorrect username or password' })
       return
     }
     const user = await User.findById(decodedToken.id)
@@ -59,6 +59,68 @@ blogsRouter.post('/', async (req, res, next) => {
       res.status(400).send(`Value for ${e.stringValue} is not correct`)
     } else {
       next(e)
+    }
+  }
+})
+/**DELETE a post with by id, Ex-4.21*/
+
+blogsRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const decodedToken = await jwt.verify(
+      tokenExtractor.tokenExtractor(req),
+      config.SECRET
+    )
+    console.log(tokenExtractor.tokenExtractor(req))
+    if (!tokenExtractor.tokenExtractor(req) || !decodedToken.id) {
+      res.status(400).send({ error: 'Incorrect username or password' })
+      return
+    }
+    const delThisBlog = await Blog.findById(req.params.id)
+
+    //console.dir(delThisBlog);
+    if (delThisBlog.user.toString() === decodedToken.id.toString()) {
+      await Blog.findByIdAndRemove(req.params.id)
+      res.status(204).end()
+    } else {
+      res.status(400).end()
+    }
+  } catch (e) {
+    if (e.name === 'CastError') {
+      console.log(e)
+      res.status(400).send(`Id: ${e.stringValue} does not exist`)
+    } else {
+      next(e)
+    }
+  }
+})
+
+/**UPDATE a post by id, Ex-4.14*/
+
+blogsRouter.put('/:id', async (req, res, next) => {
+  const body = req.body
+  const updateBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+  if (!body.title || !body.url || !body.author) {
+    res.status(400).json({ error: 'missing fields' })
+  } else if (!body.likes) {
+    updateBlog.likes = 0
+  } else if (isNaN(Number(body.likes))) {
+    res.status(400).json({ error: 'value of like should be a number' })
+  } else {
+    try {
+      await Blog.findByIdAndUpdate(req.params.id, updateBlog)
+      res.status(201).send()
+    } catch (e) {
+      if (e.name === 'CastError') {
+        console.log(e)
+        res.status(400).send(`${e.value} is not correct id`)
+      } else {
+        next(e)
+      }
     }
   }
 })
